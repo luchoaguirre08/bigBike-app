a436bc1ffbab627880d62511526b4aba59435392
luiaguir <luiaguir@bancolombia.com.co>
2025-04-04 19:26:47 -0500

    Se realizan cambios en botones de imprimir y enviar

--- src/app/pages/registro-cliente/registro-cliente.component.ts
+++ src/app/pages/registro-cliente/registro-cliente.component.ts
@@ -78,7 +78,7 @@ export class RegistroClienteComponent {
     const numero = phone;
 
     const mensaje = encodeURIComponent(
-      `🚴‍♂️ ¡Hola ${this.ultimoClienteRegistrado.name}!\n\nGracias por registrarte en BigBike Workshop.\n\nAquí tienes tu código QR para futuras citas y ver tu historial: ${this.ultimoClienteRegistrado.qrUrl}\n\n✅ Guarda esta imagen para usarla en nuestros servicios.`
+      `🚴‍♂️ ¡Hola ${this.ultimoClienteRegistrado.name}!\n\nGracias por registrarte en Big Bike Workshop.\n\nAquí tienes tu código QR para futuras citas y ver tu historial: ${this.ultimoClienteRegistrado.qrUrl}\n\n✅ Guarda esta imagen para usarla en nuestros servicios.`
     );
 
     const url = `https://wa.me/57${numero}?text=${mensaje}`;
