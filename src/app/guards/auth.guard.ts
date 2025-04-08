f166730caf11a1bf85b9b4c6b7d29fc03c21c844
luiaguir <luiaguir@bancolombia.com.co>
2025-04-07 22:54:19 -0500

    Se realizan ajustes en clientes registrados y cargue de login repetido, de igual manera se ajusta edicion de usuarios

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,11 +1,24 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 
-export const authGuard: CanActivateFn = () => {
-  const auth = inject(AuthService);
+export const authGuard: CanActivateFn = async () => {
   const router = inject(Router);
-  if (auth.isLoggedIn()) return true;
-  router.navigate(['/login']);
-  return false;
+  const auth = getAuth();
+
+  // Esperar a que Firebase diga si hay usuario o no
+  const user = await new Promise((resolve) => {
+    const unsub = onAuthStateChanged(auth, (user) => {
+      unsub(); // Evita múltiples llamados
+      resolve(user);
+    });
+  });
+
+  if (user) {
+    return true;
+  } else {
+    router.navigate(['/login']);
+    return false;
+  }
 };
